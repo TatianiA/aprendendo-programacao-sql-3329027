7eb687110aa22c433b1ee96a107806eb5732e0f2
TatianiA <tatiani.almeida19@gmail.com>
2026-05-21 12:29:40 -0300

    Terminou Grud + Operadores

--- exercicios/1-CRUD/5-atualizar-dados.sql
+++ exercicios/1-CRUD/5-atualizar-dados.sql
@@ -4,6 +4,12 @@ SET id_salario = 3
 Where EmployeeId = 6;
 
 -- Atualize a classe salarial e o LastName da pessoa cujo EmployeeId é igual a 2. Lembre-se que a classe salarial deve corresponder o id_salario da tabela salario_classe
-
+UPDATE employees
+SET id_salario = 3,
+LastName = "Silva"
+Where EmployeeId = 2;
 
 -- Atualize a classe salarial 1 para as pessoas cuja coluna id_salario continua NULL. Lembre-se que a classe salarial deve corresponder o id_salario da tabela salario_classe
+UPDATE employees
+SET id_salario = 1
+Where id_salario is null;
--- exercicios/2-construindo-consultas/1-operadores.sql
+++ exercicios/2-construindo-consultas/1-operadores.sql
@@ -1,6 +1,33 @@
+Operadores
+Aritméticos
+Logicos
+Relacionais
+
 -- Construa uma consulta para a tabela "invoice_items" que retorne apenas as colunas InvoiceId, TrackId e UnitPrice.
-
+SELECT
+TrackID,
+UnitPrice
+FROM
+Invoice_items;
 -- Inclua nessa consulta mais três colunas, cada uma como resultado de uma operação aritmética diferente
-
+SELECT
+TrackID,
+UnitPrice,
+UnitPrice * 3,
+UnitPrice + 8,
+(UnitPrice + 8) - 5
+FROM
+Invoice_items
+WHERE InvoiceId != 5
 -- Incremente essa consulta utilizando a cláusula WHERE, a qual restringirá o resultado apenas para linhas cujo o InvoiceId seja maior do que 20 e menor ou igual a 30
-
+SELECT
+TrackID,
+UnitPrice,
+UnitPrice * 3,
+UnitPrice + 8,
+(UnitPrice + 8) - 5
+FROM
+Invoice_items
+WHERE 
+InvoiceId > 20
+AND InvoiceId <= 30;
