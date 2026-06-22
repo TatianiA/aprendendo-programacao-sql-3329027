6a82aef9ea35f37a7e2c7e62745fc4862ed2cbd0
TatianiA <tatiani.almeida19@gmail.com>
2026-06-22 13:19:08 -0300

    Estudos 22/06

--- exercicios/4-refinando-consulta/1-filtro.sql
+++ exercicios/4-refinando-consulta/1-filtro.sql
@@ -1,1 +1,13 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists". Além disso, apresente apenas as músicas do artista que contenha "Nação" no nome e título do álbum que não seja "Da Lama Ao Caos"
+SELECT
+tra.TrackId AS id,
+tra.Name AS musica,
+alb.Title AS titulo_artista,
+art.Name AS artista
+FROM
+tracks AS tra
+INNER JOIN albums as alb on tra.AlbumId = alb.AlbumId
+INNER JOIN artists as art on art.ArtistId = alb.ArtistId
+WHERE
+artista like '%nação%'
+AND titulo_artista NOT LIKE 'Da Lama ao Caos';
--- exercicios/4-refinando-consulta/2-group-by-e-having.sql
+++ exercicios/4-refinando-consulta/2-group-by-e-having.sql
@@ -1,7 +1,39 @@
 -- Crie uma consulta usando as tabelas "invoices" e "customers" cujo o resultado seja o total de gastos por cliente
-
+SELECT
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+sum(inv.Total) AS total_gasto,
+Count(inv.Total) As quant_compras
+FROM
+invoices AS inv
+INNER JOIN customers AS cus on inv.CustomerId = cus.CustomerId
+Group By id_cliente
+HAVING SUM(inv.Total) <=40;
 
 -- Na consulta anterior, insira um filtro para que apresente apenas os clientes que gastaram mais de R$30
-
+SELECT
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+sum(inv.Total) AS total_gasto,
+Count(inv.Total) As quant_compras
+FROM
+invoices AS inv
+INNER JOIN customers AS cus on inv.CustomerId = cus.CustomerId
+Group By id_cliente
+HAVING SUM(inv.Total) >=30;
 
 -- Na consulta anterior, insira uma coluna que informe a compra de maior valor feita por cada cliente
+SELECT
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+sum(inv.Total) AS total_gasto,
+Count(inv.Total) As quant_compras,
+ MAX(inv.Total) AS maior_compra
+FROM
+invoices AS inv
+INNER JOIN customers AS cus on inv.CustomerId = cus.CustomerId
+Group By cus.CustomerId,
+cus.FirstName
+ORDER BY
+total_gasto DESC
+LIMIT 1;
